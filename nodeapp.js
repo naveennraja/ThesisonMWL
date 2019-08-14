0d3b45a7ac188997b1bddc66088df3ac25fa62dc
Naveen <naveennraja@gmail.com>
2019-08-14 16:28:48 +0100

    Hosting of simple  task

--- nodeapp.js
+++ nodeapp.js
@@ -15,7 +15,8 @@ app.use(function(req, res, next) {
     next();
 });
 // Set static directory before defining routes
-app.use(express.static(path.join(__dirname, 'public')));
+console.log("PWD-----------------------------------",__dirname);
+app.use(express.static(path.join(__dirname)));
 app.use("/node_modules", express.static(__dirname + '/node_modules'));
 // Enable parsing of posted forms
 app.use(bodyParser.urlencoded({ extended: false }));
--- public/angular/angular-app.js
+++ public/angular/angular-app.js
@@ -3,34 +3,34 @@ function config($routeProvider, $locationProvider) {
     $routeProvider.when('/', {
         controller: introductionController,
         controllerAs: 'vm',
-        templateUrl: "angular/introduction-page/introduction.html"
+        templateUrl: "public/angular/introduction-page/introduction.html"
     }).when('/simple-task',{
         controller : simpleTaskController,
-        templateUrl:"angular/simple-task/simple-task.html",
+        templateUrl:"public/angular/simple-task/simple-task.html",
         controllerAs : 'vm'
     }).when('/nasa-tlx',{
-            templateUrl:"angular/nasa-tlx/nasa-tlx.html",
+            templateUrl:"public/angular/nasa-tlx/nasa-tlx.html",
             controller : nasatlxcontroller,
             controllerAs : 'vm'
     }).when('/self-report-measure',{
-            templateUrl:"angular/self-report-measure/self-report-measure.html",
+            templateUrl:"public/angular/self-report-measure/self-report-measure.html",
             controller : selfreportmeasure,
             controllerAs : 'vm'
     }).when('/medium-task',{
-        templateUrl:"angular/medium-task/medium-task.html",
+        templateUrl:"public/angular/medium-task/medium-task.html",
         controller : mediumTaskController,
         controllerAs : 'vm'
     }).when('/complex-task',{
-        templateUrl:"angular/complex-task/complex-task.html",
+        templateUrl:"public/angular/complex-task/complex-task.html",
         controller : complexTaskController,
         controllerAs : 'vm'
     }).when('/thank-you',{
-        templateUrl:"angular/thank-you/thank-you.html",
+        templateUrl:"public/angular/thank-you/thank-you.html",
         skipLocationChange: false
      }).otherwise({
         controller: introductionController,
         controllerAs: 'vm',
-        templateUrl: "angular/introduction-page/introduction.html"
+        templateUrl: "public/angular/introduction-page/introduction.html"
 
     });
     $locationProvider.html5Mode(false).hashPrefix('!');
